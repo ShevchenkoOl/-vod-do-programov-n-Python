fd9ea28d39aaec0a71a56b1a51bf4460673f3003
Oleksii Shevchenko <uzlabini@gmail.com>
2023-11-30 08:03:31 +0100

    created lekce1

---
+++ program.py
@@ -0,0 +1,24 @@
+#----------------------------------------Lekce 1 Čísla a řetězce-----------------------------------
+'''
+Aritmetické operátory
+sčítání: +
+odčítání: -
+násobení: *
+dělení: /
+mocnění: **                возводит в степень 2 ** 3 (2 в 3 степени) zvyšuje na sílu 2 ** 3 (2 na 3)
+celočíselné dělení: //     делит числа без остатка 7 // 2 = 3
+zbytek po dělení: %        vzít zbytek dělení 7 / 2, abyste dostali celé číslo 3 a zbyde nám 1 (2*3=6 7-6=1)
+'''
+print("Hello, World!")
+print(12 * 13 + 10)
+
+a = 12
+b = 13
+print(a ** b)
+
+print(7 / 2)
+print(7 // 2)
+print(7 % 2)
+
+print(3.14)  # 3.14 desetinná čísla vždycky píší s tečkou, nikoliv s čárkou.
+print(3,14)  # 3 14
